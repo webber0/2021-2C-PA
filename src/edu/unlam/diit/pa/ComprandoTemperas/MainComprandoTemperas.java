837b99b33391c16d92294bba168acc5e2fe31c9c
nicol <nicol@DESKTOP-PVJHDJP>
2022-05-25 15:54:07 -0300

    Se agrega enunciado

--- src/edu/unlam/diit/pa/ComprandoTemperas/MainComprandoTemperas.java
+++ src/edu/unlam/diit/pa/ComprandoTemperas/MainComprandoTemperas.java
@@ -1,4 +1,16 @@
 package edu.unlam.diit.pa.ComprandoTemperas;
+/*
+Marilén quiere ayudar a su hijo con una tarea del jardín de infantes. 
+Le han pedido que lleve materiales para pintar una serie de formas geométricas con colores. 
+Habrá un grupo de formas que pintará de un color, y otro grupo que pintará de otro color, 
+y así sucesivamente. Ella dispone por anticipado de las medidas y ubicaciones precisas de cada figura, 
+pero necesita saber cuántos pomos de cada témpera debe comprar.
+Sabiendo que un pomo de témpera tiene un poder cubritivo de 100cm2, 
+y que no puede comprar "fracciones de pomo", 
+se te pide que le muestres una forma de calcular las cantidades a comprar utilizando el patrón de diseño Composite.
+
+Nota: Las figuras no se solapan entre sí. Cada figura estará pintada al 100% del color indicado.
+ */
 
 public class MainComprandoTemperas {
 
